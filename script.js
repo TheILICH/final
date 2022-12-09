eca190b7f7ca4c1fc586cff8e4014f205226e2ef
dillekk <210103322@stu.sdu.edu.kz>
2022-12-10 01:29:02 +0600

    another animation

--- script.js
+++ script.js
@@ -14,3 +14,49 @@ document.addEventListener('scroll', function(e) {
       
     });
 });
+
+var number = document.querySelector('#number'),
+        numberTop = number.getBoundingClientRect().top,
+        start = +number.innerHTML, end = +number.dataset.max;
+
+        window.addEventListener('scroll', function onScroll() {
+            if(window.pageYOffset > numberTop - window.innerHeight / 2) {
+                this.removeEventListener('scroll', onScroll);
+                var interval = setInterval(function() {
+                    number.innerHTML = ++start;
+                    if(start == end) {
+                        clearInterval(interval);
+                    }
+                }, 5);
+            }
+        });
+        var number1 = document.querySelector('#number1'),
+        numberTop1 = number1.getBoundingClientRect().top,
+        start1 = +number1.innerHTML, end1 = +number1.dataset.max;
+
+        window.addEventListener('scroll', function onScroll() {
+            if(window.pageYOffset > numberTop1 - window.innerHeight / 2) {
+                this.removeEventListener('scroll', onScroll);
+                var interval1 = setInterval(function() {
+                    number1.innerHTML = ++start1;
+                    if(start1 == end1) {
+                        clearInterval(interval1);
+                    }
+                }, 5);
+            }
+        });
+        var number2 = document.querySelector('#number2'),
+        numberTop2 = number2.getBoundingClientRect().top,
+        start2 = +number2.innerHTML, end2 = +number2.dataset.max;
+
+        window.addEventListener('scroll', function onScroll() {
+            if(window.pageYOffset > numberTop2 - window.innerHeight / 2) {
+                this.removeEventListener('scroll', onScroll);
+                var interval2 = setInterval(function() {
+                    number2.innerHTML = ++start2;
+                    if(start2 == end2) {
+                        clearInterval(interval2);
+                    }
+                }, 5);
+            }
+        });
